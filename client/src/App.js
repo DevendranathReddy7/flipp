fbbe1fda354080d5416bfd45997afc22e05853d7
Devendra <devendranathreddy54@gmail.com>
2023-12-13 16:41:53 +0530

    started with Header & footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 const App = () => {
   return (
     <div>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to Flipp</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   )
 }
