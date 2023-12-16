3a192357c286a521c4c939ed97d294d12b85830a
Devendra <devendranathreddy54@gmail.com>
2023-12-16 21:41:38 +0530

    setting custom error

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,22 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js';
+import productsRoutes from './routes/productsRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
-import { product } from './data/Products.js'
+
+
 connectDB()
 
 const app = express()
 
-app.get('/api/products', (req, res) => {
-    res.json(product)
-})
+app.get('/', (req, res) => {
+    res.send('API running....')
+});
 
-app.get('/api/products/:prdid', (req, res) => {
-    const prd = product.find(prd => prd._id === req.params.prdid)
-    res.json(prd)
-})
+app.use('/api/products', productsRoutes)
+app.use(notFound)
+app.use(errorHandler)
+
 
 app.listen(port, () => console.log(`Server started on port:${port}`))
