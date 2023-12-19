1addf85e6d37d1330e4b943d0268cf68285446fe
Devendra <devendranathreddy54@gmail.com>
2023-12-19 21:36:35 +0530

    creating auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, regUser, logout, getUserProfile, getUsers, deleteUser, getUserById, updateUser } from '../controllers/userController'
+import { authUser, regUser, logout, getUserProfile, getUsers, deleteUser, getUserById, updateUser } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').post(regUser).get(getUsers)
+router.route('/').post(regUser).get(protect, admin, getUsers)
 router.post('/logout', logout)
 router.post('/login', authUser)
-router.route('/profile').get(getUserProfile).put(updateUser)
-router.route('/:prdid').delete(deleteUser).get(getUserById).put(updateUser)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUser)
+router.route('/:prdid').delete(protect, admin, deleteUser).get(protect, admin, getUserById)
+    .put(protect, admin, updateUser)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,19 @@ dotenv.config()
 import connectDB from './config/db.js';
 import productsRoutes from './routes/productsRoutes.js'
 import userRoutes from './routes/userRoutes.js'
-
+import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 const port = process.env.PORT || 5000;
 
 
 connectDB()
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.send('API running....')
